939e6b5d528fc0a8ece2c994d52e6fd35a1c4d16
bennettoh <bennettoh.arch@gmail.com>
2020-02-11 16:25:41 -0800

    set up grid and orbit control

---
+++ README.md
@@ -0,0 +1,27 @@
+Web3D
+========
+
+[![Dependencies][dependencies]][dependencies-url]
+[![Language Grade][lgtm]][lgtm-url]
+
+A three.js project that automates simple building design.
+
+Task list
+
+- [x] Initialize workspace
+- [ ] Take input points to draw boxes
+- [ ] Interactive UI
+- [ ] Camera movement
+- [ ] Object selection to UI
+- [ ] Custom texture settings
+- [ ] FPS monitor
+
+
+#### three.js ####
+
+three.js is a JavaScript 3D library to create an easy to use, lightweight, 3D library with a default WebGL renderer. The library also provides Canvas 2D, SVG and CSS3D renderers in the examples.
+
+[dependencies]: https://img.shields.io/david/mrdoob/three.js.svg
+[dependencies-url]: https://david-dm.org/mrdoob/three.js
+[lgtm]: https://img.shields.io/lgtm/grade/javascript/g/mrdoob/three.js.svg?label=code%20quality
+[lgtm-url]: https://lgtm.com/projects/g/mrdoob/three.js/
--- app.js
+++ app.js
@@ -1,48 +1,185 @@
-import * as THREE from './js/three.module.js'
+import { OrbitControls } from '/jsm/controls/OrbitControls.js';
 
-import Stats from './jsm/libs/stats.module.js';
+		var camera, scene, renderer;
+		var plane;
+		var mouse, raycaster, isShiftDown = false;
 
-import { OrbitControls } from './jsm/controls/OrbitControls.js';
-import { GLTFLoader } from './jsm/loaders/GLTFLoader.js';
-import { DRACOLoader } from './jsm/loaders/DRACOLoader.js';
+		var rollOverMesh, rollOverMaterial;
+		var cubeGeo, cubeMaterial;
 
-var camera, scene, renderer;
-var geometry, material, mesh;
-// var controls;
+		var objects = [];
 
-init();
-animate();
+		init();
+		render();
 
-function init() {
+		var controls = new OrbitControls(camera, renderer.domElement);
+		controls.update();
+		controls.target.set(0, 0, 0);
+		controls.enablePan = false;
 
-  camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 100);
-  camera.position.set(0, 0, 8);
+		function init() {
 
-  // controls = new OrbitControls(camera, renderer.domElement);
-  // controls.target.set(0, 0.5, 0);
-  // controls.enablePan = false;
+			camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+			camera.position.set(500, 800, 1300);
+			camera.lookAt(0, 0, 0);
 
-  scene = new THREE.Scene();
+			scene = new THREE.Scene();
+			scene.background = new THREE.Color(0xf0f0f0);
 
-  geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-  material = new THREE.MeshNormalMaterial();
+			// roll-over helpers
 
-  mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+			var rollOverGeo = new THREE.BoxBufferGeometry(50, 50, 50);
+			rollOverMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, opacity: 0.5, transparent: true });
+			rollOverMesh = new THREE.Mesh(rollOverGeo, rollOverMaterial);
+			scene.add(rollOverMesh);
 
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+			// cubes
 
-}
+			cubeGeo = new THREE.BoxBufferGeometry(50, 50, 50);
+			cubeMaterial = new THREE.MeshNormalMaterial();
 
-function animate() {
+			// grid
 
-  requestAnimationFrame(animate);
+			var gridHelper = new THREE.GridHelper(1000, 20);
+			scene.add(gridHelper);
 
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.02;
+			//
 
-  renderer.render(scene, camera);
+			raycaster = new THREE.Raycaster();
+			mouse = new THREE.Vector2();
 
-}
+			var geometry = new THREE.PlaneBufferGeometry(1000, 1000);
+			geometry.rotateX(- Math.PI / 2);
+
+			plane = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ visible: false }));
+			scene.add(plane);
+
+			objects.push(plane);
+
+			// lights
+
+			var ambientLight = new THREE.AmbientLight(0x606060);
+			scene.add(ambientLight);
+
+			var directionalLight = new THREE.DirectionalLight(0xffffff);
+			directionalLight.position.set(1, 0.75, 0.5).normalize();
+			scene.add(directionalLight);
+
+			renderer = new THREE.WebGLRenderer({ antialias: true });
+			renderer.setPixelRatio(window.devicePixelRatio);
+			renderer.setSize(window.innerWidth, window.innerHeight);
+			document.body.appendChild(renderer.domElement);
+
+			document.addEventListener('mousemove', onDocumentMouseMove, false);
+			document.addEventListener('mousedown', onDocumentMouseDown, false);
+			document.addEventListener('keydown', onDocumentKeyDown, false);
+			document.addEventListener('keyup', onDocumentKeyUp, false);
+
+			//
+
+			window.addEventListener('resize', onWindowResize, false);
+
+		}
+
+		function onWindowResize() {
+
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix();
+
+			renderer.setSize(window.innerWidth, window.innerHeight);
+
+		}
+
+		function onDocumentMouseMove(event) {
+
+			event.preventDefault();
+
+			mouse.set((event.clientX / window.innerWidth) * 2 - 1, - (event.clientY / window.innerHeight) * 2 + 1);
+
+			raycaster.setFromCamera(mouse, camera);
+
+			var intersects = raycaster.intersectObjects(objects);
+
+			if (intersects.length > 0) {
+
+				var intersect = intersects[0];
+
+				rollOverMesh.position.copy(intersect.point).add(intersect.face.normal);
+				rollOverMesh.position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
+
+			}
+
+			render();
+
+		}
+
+		function onDocumentMouseDown(event) {
+
+			event.preventDefault();
+
+			mouse.set((event.clientX / window.innerWidth) * 2 - 1, - (event.clientY / window.innerHeight) * 2 + 1);
+
+			raycaster.setFromCamera(mouse, camera);
+
+			var intersects = raycaster.intersectObjects(objects);
+
+			if (intersects.length > 0) {
+
+				var intersect = intersects[0];
+
+				// delete cube
+
+				if (isShiftDown) {
+
+					if (intersect.object !== plane) {
+
+						scene.remove(intersect.object);
+
+						objects.splice(objects.indexOf(intersect.object), 1);
+
+					}
+
+					// create cube
+
+				} else {
+
+					var voxel = new THREE.Mesh(cubeGeo, cubeMaterial);
+					voxel.position.copy(intersect.point).add(intersect.face.normal);
+					voxel.position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
+					scene.add(voxel);
+
+					objects.push(voxel);
+
+				}
+
+				render();
+
+			}
+
+		}
+
+		function onDocumentKeyDown(event) {
+
+			switch (event.keyCode) {
+
+				case 16: isShiftDown = true; break;
+
+			}
+
+		}
+
+		function onDocumentKeyUp(event) {
+
+			switch (event.keyCode) {
+
+				case 16: isShiftDown = false; break;
+
+			}
+
+		}
+
+		function render() {
+
+			renderer.render(scene, camera);
+
+		}
